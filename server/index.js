69d884177b82bd94c28aaf136caa8f538957b939
Sparky Gaming <irsahmd07@gmail.com>
2024-03-31 19:59:33 +0530

    signup commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import userRoutes from './routes/users.js';
+
 const app = express();
 app.use(express.json({limit:"30mb", extended: true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 5005
 app.get('/',(req,res) => {
     res.send("This is a stack overflow clone API")
 })
+
+app.use('/user', userRoutes)
 
 const Connection_URL = "mongodb+srv://Irshad:Irshad7865@stack-overflow-clone.xtgnlnb.mongodb.net/?retryWrites=true&w=majority&appName=stack-overflow-clone"
 
