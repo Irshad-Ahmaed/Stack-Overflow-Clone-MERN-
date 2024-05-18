2a7d742afe085d046f8e44d9ef2ea3937e1d5188
Sparky Gaming <irsahmd07@gmail.com>
2024-05-18 11:51:36 +0530

    Auth enable

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import Logo from '../../assets/SO_logo.svg';
 import search from '../../assets/search.svg';
 import Avatar from '../../components/Avatar/Avatar';
@@ -8,16 +8,35 @@ import "./Navbar.css";
 import {useSelector, useDispatch} from 'react-redux'
 import { setCurrentUser } from '../../actions/currentUser';
 
+import { jwtDecode } from "jwt-decode";
+
 const Navbar = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   let User = useSelector((state) => (state.currentUserReducer));
 
+  const handleLogout = () => {
+    dispatch({type: 'LOGOUT'});
+    navigate('/')
+    dispatch(setCurrentUser(null))
+  }
+
   useEffect(() => {
+    const token = User?.token
+    
+    if (token) {
+      const decodeToken = jwtDecode(token)
+      if (decodeToken.exp * 1000 < new Date().getTime()) {
+        console.log("Time out!")
+        handleLogout()
+      }
+    }
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem('Profile'))))
 
   }, [dispatch])
     
+  
 
   return (
     <nav className='main-nav'>
@@ -51,7 +70,7 @@ const Navbar = () => {
                 </> :
                 <>
                   <Link to='/User' style={{margin:"0"}} className='nav-links avatarLogo'><Avatar backgroundColor='#009dff' fontSize="20px" px="5px" py="15px" borderRadius='50%' color='white'>{User.result.name.charAt(0).toUpperCase()}</Avatar></Link>
-                  <button className='nav-item nav-links' style={{margin:"0 50px 0 0px"}}>Log out</button>
+                  <button className='nav-item nav-links' onClick={handleLogout} style={{margin:"0 50px 0 0px"}}>Log out</button>
                 </>
             }
 
--- server/routes/Answers.js
+++ server/routes/Answers.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
 import { deleteAnswer, editAnswer, postAnswer } from '../controllers/Answers.js'
+import auth from '../middlewares/auth.js'
 
 const router = express.Router();
 
-router.patch('/post/:id', postAnswer)
-router.patch('/deleteAnswer/:id', deleteAnswer);
-router.patch('/updateAnswer/:id', editAnswer);
+router.patch('/post/:id', auth, postAnswer)
+router.patch('/deleteAnswer/:id', auth, deleteAnswer);
+router.patch('/updateAnswer/:id', auth, editAnswer);
 
 export default router;
